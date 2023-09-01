c9d6790d20cd1b144aa9b0473ebfb732a22ec7d1
faissal17 <faissalaoukacha@gmail.com>
2023-09-01 17:23:31 +0100

    start the working on the java project

---
+++ src/BookManager.java
@@ -0,0 +1,5 @@
+class BookManager{
+    public static void main(String[] args) {
+        
+    }
+}
--- src/DbConnection.java
+++ src/DbConnection.java
@@ -1,12 +1,12 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class Main {
+public class DbConnection {
     public static void main(String[] args) {
         try {
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "root","");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "root", "");
             System.out.println(con);
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("NO Connection Was made");
         }
     }
